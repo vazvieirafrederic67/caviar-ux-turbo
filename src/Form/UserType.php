bad9760586d7f374f8195439d2f87fb65d66afb3
vazvieirafrederic67 <kingface@hotmail.fr>
2021-08-02 06:39:45 +0200

    update

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class UserType extends AbstractType
 {
